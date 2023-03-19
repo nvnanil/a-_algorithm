9ef6fc9a8de3cc82111ea5b913e3a3dddd9531c9
nvnanil <121992602+nvnanil@users.noreply.github.com>
2023-03-19 00:01:46 -0400

    Woekspace and dictionary updation
    Created workspace and dictionary for storing information.

--- a_star_naveen_orlandis.py
+++ a_star_naveen_orlandis.py
@@ -2,16 +2,27 @@ import numpy as np
 import math
 import cv2 as cv
 from math import dist
+import time
 
-#Creating workspace
+#Map Space
 map = np.ones((250, 600, 3), np.uint8)*255
 thresh = 0.5
 angle_thresh = 30
-x_grid = np.arange(0, 400, thresh)
-y_grid = np.arange(0, 250, thresh)
+x_axis = np.arange(0, 600, thresh)
+y_axis = np.arange(0, 250, thresh)
 theta_grid = np.arange(0, 360, 30)
+###################################Defining workspace
+b_canvas = np.zeros((250,600,3),np.uint8)
+#Puffing Walls
+for i in range(5):
+    for j in range(b_canvas.shape[0]):
+        for k in range(b_canvas.shape[1]):
+            b_canvas[j][i] = (0,0,255)
+            b_canvas[i][k] = (0,0,255)
+            b_canvas[b_canvas.shape[0]-1-i][k] = (0,0,255)
+            b_canvas[j][b_canvas.shape[1]-1-i] = (0,0,255)
 
-#Creating nodes and storing in a dictionary
+####################################Creating nodes and storing in a dictionary
 def get_node( pos, theta, parent, cost):
     Node  = {'pos': pos,
              'theta': theta, 
@@ -21,32 +32,42 @@ def get_node( pos, theta, parent, cost):
 
 def initial_nodes(start_key):
   open_dict = {}
-  for x in x_grid: 
-    for y in y_grid:  
+  for x in x_axis: 
+    for y in y_axis:  
         pos = (x, y)
         for theta in theta_grid:  
             open_dict[(pos, theta)] = get_node(pos, theta, None, np.inf)
-  open_dict[start_key]['cost'] = 0
+  open_dict[start_key]['cost'] = 0 #Assigning initial cost to be zero
   return open_dict
-
+################################Goal check
 def check_goal(child_node, goal_pos, theta_goal):
     dst = dist(child_node['pos'], goal_pos) #Calculating Euclidean distance
     
     dtheta = np.abs(child_node['theta'] - theta_goal)
-    # print(dtheta, child_node['theta'], theta_goal)
     if dst < 1.5 and dtheta <= 30: #Goal reached
         return  True
     else: 
         return False
     
-    #Entering the input values
 while ip:
+
     print("---------------------------------------------------------")
     start_x= int(input("Enter the x coordinate of the start point: "))
     start_y= int(input("Enter the y coordinate of the start ponit: "))
+    theta_s = int(input("Enter the orientation of the start point: "))
     goal_x= int(input("Enter the x coordinate of the goal point: "))
     goal_y= int(input("Enter the y coordinate of the goal point: "))
+    theta_g= int(input("Enter the orientation of the goal point: "))
+    K = int(input("Enter the length of step: "))
 
+    if (theta_s/30 != 0) and theta_s not in range (-60, 61):
+        print('Invalid entry')
+        print("Try again")
+    elif (theta_g/30 != 0) and theta_s not in range (-60, 61):
+        print("Invalid entry")
+        print("Try again")
+    elif K not in range(0,11):
+        print("Enter K in the range of 1 to 10; Try again")
     if (start_x > b_canvas.shape[1] or start_y > b_canvas.shape[0] or goal_x > b_canvas.shape[1] or goal_y > b_canvas.shape[0]): #Checking whether outside the work space
         print("Invalid input, entered value outside the path space")
         print("Try Agian")
@@ -55,3 +76,11 @@ while ip:
         print("Try Again")
     else:
         ip = False
+    
+    s_node = (start_x,start_y)
+    g_node = (goal_x, goal_y)
+    s_node_ = (s_node,theta_s)
+    nodes = initial_nodes(s_node)
+
+    
+    
