2700869585678408dc6fb65ea5fbc3612a45f4ea
nvnanil <121992602+nvnanil@users.noreply.github.com>
2023-03-20 01:28:27 -0400

    Final modified code
    Code with final modifications.

--- a_star_naveen_orlandis.py
+++ a_star_naveen_orlandis.py
@@ -3,13 +3,11 @@ import math
 import cv2
 from math import dist
 import time
-from math import dist
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')                    
-out = cv2.VideoWriter('w_space.avi', fourcc, 30.0, (600, 250))
+out = cv2.VideoWriter('project_3.avi', fourcc, 30.0, (600, 250))
 
 #Creating work space
-#map = np.ones((250, 600, 3), np.uint8)*255
 b_canvas = np.zeros((250,600,3),np.uint8)
 width = 600
 height = 250
@@ -18,11 +16,11 @@ thresh = 0.5
 angle_thresh = 30
 x_axis = np.arange(0, 600, thresh)
 y_axis = np.arange(0, 250, thresh)
-theta_grid = np.arange(0, 360, 30)
+theta_grid = np.arange(0, 360, angle_thresh)
 theta_move = [-60, -30, 0, 30, 60]
 
 ####################################Creating obstacle space
-def w_space(max_x,max_y):
+def w_space(max_x,max_y, clear):
     #Storing points of the array into an empty list
     all_points = []
     obstacle_space = []
@@ -32,17 +30,29 @@ def w_space(max_x,max_y):
     for e in all_points:
         x = e[1]
         y = e[0]
-    #Storing the points of the obstacle space
     #Defining obstacles using Half - Plane equations
     #Lower rectangle
-        if y>=95 and y<=155 and x>=0 and x<=105:
+        if y>=(100-clear) and y<=(150+clear) and x>=0 and x<=(100+clear):
             obstacle_space.append((x,y))
-        elif y>=95 and y<=155 and x>=145 and x<=250: #Upper rectangle
+        elif y>=(100-clear) and y<=(150+clear) and x>=(150-clear) and x<=250: #Upper rectangle
             obstacle_space.append((x,y))
-        elif x >= 1.75*y - 776.25 and x <= -1.75*y + 1026.25 and y >= 455: #Triangle
+        elif (y >= (460 - clear)) and (x >= (25 - clear)) and (x <= (225 + clear)) and ((-2*y + x) >= -895 + clear) and ((2*y + x) <= 1145 - clear): #Triangle
             obstacle_space.append((x,y))
-        elif (y >= (235 - 5)) and (y <= (365 + 5)) and ((y + 2*x) >= 395) and ((y - 2*x) <= 205) and ((y - 2*x) >= -105) and ((y + 2*x) <= 705): #Hexagon
+        elif (y >= (235 - clear)) and (y <= (365 + clear)) and ((y + 2*x) >= 390 - clear) and ((y - 2*x) <= 210 + clear) and ((y - 2*x) >= -130 + clear) and ((y + 2*x) <= 710 + clear):
             obstacle_space.append((x,y))
+    #Puffing the walls
+    for i in range(clear):
+        for j in range(b_canvas.shape[0]):
+            for k in range(b_canvas.shape[1]):
+                b_canvas[j][i] = (0,0,255)
+                b_canvas[i][k] = (0,0,255)
+                b_canvas[b_canvas.shape[0]-1-i][k] = (0,0,255)
+                b_canvas[j][b_canvas.shape[1]-1-i] = (0,0,255)
+    for c in obstacle_space: 
+        x = c[0]
+        y = c[1]
+        b_canvas[(x,y)]=[0,0,255] #Coloring the obstacles
+
     return (obstacle_space)
 
 ####################################Creating nodes and storing in a dictionary
@@ -67,14 +77,14 @@ def is_goal(child_node, goal_pos, theta_goal):
     dst = dist(child_node['pos'], goal_pos) #Calculating Euclidean distance
     
     dtheta = np.abs(child_node['theta'] - theta_goal)
-    if dst < 1.5 and dtheta <= 30: #Goal reached
+    if dst <= 1.5 and dtheta <= 30: #Goal reached
         return  True
     else: 
         return False
     
 ##############################Back tracking
 def backtrack(b_node):
-    print("Generating path")
+    print("Generating optimal path")
     path = []
     while b_node['parent'] is not None:
         path.append(b_node)
@@ -92,23 +102,6 @@ def action(node_, theta, step):
     n_pos = ((x_//thresh)//2, (y_//thresh)//2)
     return n_pos, n_theta, node_['cost'] + 1
 
-obstacles = w_space(width,height)
-###################################Defining workspace
-#Puffing Walls
-for i in range(5):
-    for j in range(b_canvas.shape[0]):
-        for k in range(b_canvas.shape[1]):
-            b_canvas[j][i] = (0,0,255)
-            b_canvas[i][k] = (0,0,255)
-            b_canvas[b_canvas.shape[0]-1-i][k] = (0,0,255)
-            b_canvas[j][b_canvas.shape[1]-1-i] = (0,0,255)
-for c in obstacles: 
-    x = c[0]
-    y = c[1]
-    b_canvas[(x,y)]=[0,0,255] #Coloring the obstacles
-
-ch1, ch2, ch3 = cv2.split(b_canvas)
-ch3 = ch3.T
  ###########################Checking if child node in obstacle path
 def o_space(pose):
     x = int(pose[0])
@@ -117,6 +110,11 @@ def o_space(pose):
         return True
     else:
         return False
+##############################
+print("---------------------------------------------------------")
+print("Path Planner | Workspace dimensions : 250X600 pixels")
+print("Accepted orinetation of the robot : -360 to 360: as mutiples of 30")
+print("Accepted step size : 1 to 10")
     
 ip = True    
 while ip:
@@ -128,17 +126,26 @@ while ip:
     goal_x= int(input("Enter the x coordinate of the goal point: "))
     goal_y= int(input("Enter the y coordinate of the goal point: "))
     theta_g= int(input("Enter the orientation of the goal point: "))
-    k = int(input("Enter the length of step: "))
+    k = int(input("Enter the step size: "))
+    cl = int(input("Enter the clearance of the robot "))
+    r = int(input("Enter the radius of the robot " ))
 
-    if (theta_s % 30 != 0) or theta_s not in range (-60, 61):
-        print('Invalid entry')
+    clearance = cl + r
+
+    obstacles = w_space(width,height,clearance)
+    
+    ch1, ch2, ch3 = cv2.split(b_canvas)
+    ch3 = ch3.T
+
+    if (theta_s % 30 != 0):
+        print('Invalid entry of orientation')
         print("Try again")
-    elif (theta_g % 30 != 0) and theta_s not in range (-60, 61):
-        print("Invalid entry")
+    elif (theta_g % 30 != 0):
+        print("Invalid entry of orientation")
         print("Try again")
     elif k not in range(0,11):
         print("Enter K in the range of 1 to 10; Try again")
-    if (start_x > b_canvas.shape[1] or start_y > b_canvas.shape[0] or goal_x > b_canvas.shape[1] or goal_y > b_canvas.shape[0]): #Checking whether outside the work space
+    elif (start_x > b_canvas.shape[1] or start_y > b_canvas.shape[0] or goal_x > b_canvas.shape[1] or goal_y > b_canvas.shape[0]): #Checking whether outside the work space
         print("Invalid input, entered value outside the path space")
         print("Try Agian")
     elif ch3[start_x][start_y] == 255 or ch3[goal_x][goal_y] == 255: #Checking for obstacles
@@ -146,6 +153,15 @@ while ip:
         print("Try Again")
     else:
         ip = False
+
+if theta_s >= 360:
+        theta_s = theta_s - 360
+if theta_s >= -360 and theta_s < 0:
+        theta_s = theta_s + 360
+if theta_g >= 360:
+        theta_g = theta_g - 360
+if theta_g >= -360 and theta_g < 0:
+        theta_g = theta_g + 360
     
 s_node = (start_x,start_y)
 g_node = (goal_x, goal_y)
@@ -163,14 +179,11 @@ def a_star(g_node, theta_g):
         c_list.add(key)
         open_dict.pop(key)
         m_node = nodes[key]
-        #print(m_node)
         if is_goal(m_node, g_node, theta_g):
             print("Goal reached")
             return backtrack(child), create
         for t_list in theta_move:
             pos, theta, cost = action(m_node, t_list, k) #New node and cost
-            #print("Node",pos)
-            #print(theta)
 
             if not o_space(pos) and (pos,theta) not in c_list:
                 child = nodes[(pos,theta)]
@@ -182,8 +195,6 @@ def a_star(g_node, theta_g):
 
 btrack , create = a_star(g_node,theta_g)
 print('Execuion time ' + str(time.time() - start_time) + ' seconds') 
-#print("Backtracked path")
-#print(btrack)
 print("Generating video....")
 
 cv2.circle(b_canvas, (goal_x,b_canvas.shape[0]-goal_y), 2, (255, 255, 255), 2)
@@ -199,17 +210,17 @@ for n in create:
     cv2.arrowedLine(b_canvas, (int(i), int(249-j)), (int(i_), int(249-j_)), [0,250,0], 2)
     out.write(b_canvas)
 
+print("Backtracked path")
 
 if btrack is not None:
     for n in btrack:
-        print("Backtracked path")
         print(n['pos'], n['theta'])
         i, j = n['pos']
         parent = n['parent']
         if parent is None:
             parent = btrack[0]
         i_, j_ = parent['pos']
-        cv2.arrowedLine(b_canvas, (int(i), int(249-j)), (int(i_), int(249-j_)), [255,0,0], 2)
+        cv2.arrowedLine(b_canvas, (int(i), int(249-j)), (int(i_), int(249-j_)), [255,0,0], 1)
         out.write(b_canvas)
 
 out.release()
